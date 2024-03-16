dbad00bb9160316a266c9244b612e26af883a4fa
Tambe <stambe@ptc.com>
2024-03-16 20:09:24 +0530

    Added Sliders | Used Grid, theme for CSS Responsive.....

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -5,10 +5,14 @@ import { styled } from '@mui/material';
 import { bannerData } from '../../constants/data';
 
 
-const Image=styled('img')({
-    width:'100%',
-    height: 280
-})
+const Image = styled('img')(({ theme }) => ({
+    width: '100%',
+    height: 280,
+    [theme.breakpoints.down('md')]: {
+        objectFit: 'cover',
+        height: 180
+    }
+}));
 const responsive = {
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,29 +1,53 @@
 import { useEffect } from "react";
 import NavBar from "./NavBar";
 import Banner from "./Banner";
+import Slide from "./Slide";
 import { Box, styled } from "@mui/material";
 import { getProducts } from "../../redux/actions/productActions";
 import { useDispatch, useSelector } from "react-redux";
+
+
+// Component 
+
+import MidSlide from "./MidSlide";
+import MidSection from "./MidSection";
+
 const Component = styled(Box)`
     padding:10px;
     background-color: #F2F2F2;
 `
 const Home=()=>{
     //Here getProducst is from State of FrontEnd Database (Redux Table) values are stored from store.js
-    const getProducts = useSelector(state=>state.getProducts);
-    // Object destructuring Method IMP Interview questions
-    const { products }= getProducts;
+    const products = useSelector(state=>state.getProducts);
+    // // Object destructuring Method IMP Interview questions
+    // const { products }= getProduct;
     //getProducts.products
     // Dispatch getProducts before use
+    console.log(products);
+    // console.log(products.size());
     const dispatch = useDispatch();
     useEffect(()=>{
         dispatch(getProducts());
     },[dispatch])
+
     return (
         <>
             <NavBar/>
             <Component>
                 <Banner/>
+                <MidSlide products={products.products} 
+                    title="Deal of the Day"
+                    timer={true}
+                />
+                <MidSection/>
+                <Slide products={products.products} 
+                    title="Deal of the Day"
+                    timer={true}
+                />
+                <Slide products={products.products} 
+                    title="Deal of the Day"
+                    timer={true}
+                />
             </Component>
         </>
     )
